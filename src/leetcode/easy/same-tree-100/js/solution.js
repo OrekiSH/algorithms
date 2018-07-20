ea56f80ba135ffad15c0f571cba68f09fc527352
orekish <orekish@163.com>
2018-07-20 22:21:49 +0800

    update leetcode

---
+++ src/leetcode/easy/same-tree-100/js/solution.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  if (!p && !q) {
+    return true;
+  } else if (!p || !q) {
+    return false;
+  } else {
+    // else {
+    //   if () {} else {}
+    // }
+    // seem faster than
+    // else if {} else {}
+    if (p.val === q.val) {
+      return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    } else {
+      return false;
+    }
+  }
+};
