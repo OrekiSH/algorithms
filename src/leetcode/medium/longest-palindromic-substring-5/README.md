0e274c658808ac7685051bbe4c69d30cfc76b73d
orekish <orekish@163.com>
2018-07-23 21:43:04 +0800

    update leetcode

--- src/leetcode/medium/longest-palindromic-substring-5/README.md
+++ src/leetcode/medium/longest-palindromic-substring-5/README.md
@@ -2,6 +2,11 @@
 Input: "babad"
 Output: "bab" / "aba"
 
+i: 0, j: 0, current: 1, min: 0, max: 1
+i: 1 => 2, j: 1 => 0, current: 2, min: 0, max: 3
+i: 2 => 3, j: 2 => 1, current: 3, min: 0, max: 3
+i: 3, j: 3, current: 4, min: 0, max: 3
+
 Input: "cbbd"
 Output: "bb"
 ```
--- src/leetcode/medium/longest-palindromic-substring-5/js/solution.js
+++ src/leetcode/medium/longest-palindromic-substring-5/js/solution.js
@@ -8,25 +8,29 @@ var longestPalindrome = function(s) {
     return s;
   }
 
-  var start = 0;
-  var max = 0;
+  var min = 0;
+  var max = 1;
+  var current = 0;
 
-  for (var i = 0; i < len - 1; i += 1) {
-    extend(s, i, i); // odd
-    extend(s, i, i + 1); // even
+  while (current < len) {
+    var i = current;
+    var j = current;
+
+    while (i < len - 1 && s[i] == s[i + 1]) {
+      i += 1;
+    }
+    current = i + 1;
+
+    while (i < len - 1 && j > 0 && s[i + 1] == s[j - 1]) {
+      i += 1;
+      j -= 1;
+    }
+
+    let newMax = i - j + 1;
+    if (max < newMax) {
+      min = j;
+      max = newMax;
+    }
   }
-
-  return s.substring(start, start + max);
+  return s.substring(min, min + max);
 };
-
-function extend(s, i, j) {
-  while (i >= 0 && j < s.length && s[i] === s[j]) {
-    i -= 1;
-    j += 1;
-  }
-
-  if (max < j - i - 1) {
-    start = i + 1;
-    max = j - i - 1;
-  }
-}
