6b5de003878cb8aea6568f10014a32374d49931d
orekish <orekish@163.com>
2018-08-11 01:16:24 +0800

    update leetcode

---
+++ src/leetcode/medium/combination-sum-39/js/solution.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+  var result = [];
+  candidates.sort((a, b) => a - b);
+
+  if (candidates[0] > target) {
+    return result;
+  }
+  var comb = [];
+  backtrack(candidates, target, result, comb, 0);
+
+  return result;
+};
+
+function backtrack(candidates, target, result, comb, start) {
+  if (target < 0) {
+    return;
+  } else if (target === 0) {
+    return result.push(comb.slice());
+  }
+
+  for (var i = start; i < candidates.length; i += 1) {
+    comb.push(candidates[i]);
+    backtrack(candidates, target - candidates[i], result, comb, i);
+    comb.pop();
+  }
+}
