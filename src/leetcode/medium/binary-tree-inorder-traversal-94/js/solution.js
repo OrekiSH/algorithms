b272a1b14a42b6d306fd8a264a3df91498cd1000
orekish <orekish@163.com>
2018-09-10 21:13:15 +0800

    update leetcode

---
+++ src/leetcode/medium/binary-tree-inorder-traversal-94/js/solution.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  var result = [];
+  var stack = [];
+  var current = root;
+
+  while (current || stack.length) {
+    while (current) {
+      stack.push(current);
+      current = current.left;
+    }
+
+    current = stack.pop();
+    result.push(current.val);
+    current = current.right;
+  }
+
+  return result;
+};
